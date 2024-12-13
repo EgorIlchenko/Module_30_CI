c4d6a9300e30acdc5210f9517155a0de2b65e805
EgorIlchenko <egor.ilchenko93@gmail.com>
2024-12-13 15:26:55 +0300

    Add files via upload

---
+++ src/schemas.py
@@ -0,0 +1,23 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class BaseRecipe(BaseModel):
+    name: str
+    cooking_time: int
+
+
+class Recipes(BaseRecipe):
+    ingredients: str
+    description: str
+
+    class Config:
+        orm_mode = True
+
+
+class RecipeOut(BaseRecipe):
+    views: int
+    id: int
+
+    class Config:
+        orm_mode = True
