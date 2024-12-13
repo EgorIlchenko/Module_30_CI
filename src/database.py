9d69fc4a4b9a7e1e83bd9608dd7f7306e80b666c
EgorIlchenko <egor.ilchenko93@gmail.com>
2024-12-13 16:48:48 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -7,7 +7,6 @@ DATABASE_URL: str = "sqlite+aiosqlite:///./recipes.db"
 engine = create_async_engine(DATABASE_URL)
 
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-# session_local = async_session()
 Base = declarative_base()
 
 
